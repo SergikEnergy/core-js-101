055a818960a85a6694d4b25d98f7c30196452dcf
SergikEnergy <kis8883244@gmail.com>
2022-12-22 00:56:59 +0300

    add date tests completely

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -6,7 +6,6 @@
  *                                                                                           *
  ******************************************************************************************* */
 
-
 /**
  * Parses a rfc2822 string date representation into date value
  * For rfc2822 date specification refer to : http://tools.ietf.org/html/rfc2822#page-14
@@ -19,8 +18,9 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  // throw new Error('Not implemented');
+  return new Date(value);
 }
 
 /**
@@ -34,10 +34,10 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  // throw new Error('Not implemented');
+  return new Date(value);
 }
-
 
 /**
  * Returns true if specified date is leap year and false otherwise
@@ -53,10 +53,13 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  // throw new Error('Not implemented');
+  const year = date.getFullYear();
+  const daysFebruary = new Date(year, 2, 0).getDate();
+  if (daysFebruary === 29) return true;
+  return false;
 }
-
 
 /**
  * Returns the string representation of the timespan between two dates.
@@ -73,10 +76,23 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
-}
+function timeSpanToString(startDate, endDate) {
+  // throw new Error('Not implemented');
+  const diffDay = endDate.getDay() - startDate.getDay();
+  let diffHours = endDate.getHours() - startDate.getHours();
+  let diffMinutes = endDate.getMinutes() - startDate.getMinutes();
+  let diffSeconds = endDate.getSeconds() - startDate.getSeconds();
+  let diffMili = endDate.getMilliseconds() - startDate.getMilliseconds();
 
+  if (diffDay > 1) diffHours += (diffDay % 24) * 24;
+  if (diffHours < 10) diffHours = `0${diffHours}`;
+  if (diffMinutes < 10) diffMinutes = `0${diffMinutes}`;
+  if (diffSeconds < 10) diffSeconds = `0${diffSeconds}`;
+  if (diffMili < 100) diffMili = `0${diffMili}`;
+  if (diffMili < 10) diffMili = `0${diffMili}`;
+  return `${diffHours}:${diffMinutes}:${diffSeconds}.${diffMili}`;
+  // return resStr;
+}
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock
@@ -94,10 +110,25 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  // throw new Error('Not implemented');
+  let hours = date.getUTCHours();
+  let minutes = date.getMinutes();
+  const stepHour = 360 / 12;
+  const stepMinutesForHour = stepHour / 60;
+  const stepMinute = 360 / 60;
+  if (hours === 0 && minutes === 0) return 0;
+  if (hours === 12 || hours === 0) hours = 0;
+  if (hours > 12) hours %= 12;
+  if (minutes === 60 || minutes === 0) minutes = 0;
+  const angleHours = hours * stepHour + minutes * stepMinutesForHour;
+  const angleMinutes = minutes * stepMinute;
+  let diff;
+  if (angleHours > angleMinutes) diff = angleHours - angleMinutes;
+  if (angleHours < angleMinutes) diff = -angleHours + angleMinutes;
+  if (diff > 180) diff = 360 - diff;
+  return (diff * Math.PI) / 180;
 }
-
 
 module.exports = {
   parseDataFromRfc2822,
